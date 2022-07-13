0110db307a0a29996ec6a07b6e57393bae489ba0
Eric Zaleta <ericzaleta@gmail.com>
2022-07-13 03:08:12 -0500

    Door Animation and Player Movement
    Included the door animations and logic and the player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 6f;
     private Rigidbody2D rb;
     private Vector2 inputM;
+
+    private bool canInteractShop = false;
+    private bool canInteractZone = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,7 +21,52 @@ public class PlayerMovement : MonoBehaviour
         inputM.x = Input.GetAxisRaw("Horizontal");
         inputM.y = Input.GetAxisRaw("Vertical");
         inputM.Normalize();
-
+nearNPC();
+        if(canInteractShop || canInteractZone && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
         rb.velocity = inputM * speed;
     }
+
+    public void nearNPC()
+    {
+        RaycastHit2D  hit = Physics2D.CircleCast(transform.position, 2f, Vector2.zero);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.tag);
+            if (hit.collider.tag == "ShopKeeper")
+            {
+                canInteractShop = true;
+            }
+            else if (hit.collider.tag == "Zone")
+            {
+                canInteractZone = true;
+            }else
+            {
+                canInteractZone = false;
+                canInteractShop = false;
+            }
+        }
+    }
+    public void Interact()
+    {
+       if(canInteractShop)
+        {
+            Debug.Log("Interacting with Shop");
+        }
+        else if(canInteractZone)
+        {
+            Debug.Log("Interacting with Zone");
+        }
+    }
+    
+private void OnDrawGizmos()
+{
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(transform.position, 2f);
 }
+}
+
+
+
